afbe3067e3c4e4eb3d409408d2b64d5e5d8e3f28
Elias Ferreira <eliasferreira@MacBook-Pro-de-Elias.local>
2020-09-18 12:04:57 -0300

    Search Bar Friends

--- Xiritori/Controller/FriendsController.swift
+++ Xiritori/Controller/FriendsController.swift
@@ -12,11 +12,40 @@ class FriendsController: UIViewController {
 // MARK: - VIEW
     let friendsView = FriendsView()
     
+    // nav bar add button.
+    lazy var barButton: UIBarButtonItem = {
+        let icon = UIImage(systemName: "plus.circle.fill")
+        let iconSize = CGRect(origin: CGPoint.zero, size: CGSize(width: 32, height: 30))
+        let iconButton = UIButton(frame: iconSize)
+        iconButton.setBackgroundImage(icon, for: .normal)
+        iconButton.tintColor = .lightGreen
+        let barButton = UIBarButtonItem(customView: iconButton)
+        iconButton.addTarget(self, action: #selector(addFriend), for: .touchUpInside)
+        return barButton
+    }()
+    
+// MARK: - VIEW CYCLE
     override func loadView() {
         super.loadView()
         self.view = friendsView
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureNavBar()
+    }
+// MARK: - ACTIONS
+    
+    @objc func addFriend() {
+        print("Add Friend")
+    }
+// MARK: - FUNC
+    private func configureNavBar() {
+        navigationItem.title = "Lista de Amigos"
+        // Always use Light Mode.
+        navigationController?.overrideUserInterfaceStyle = .light
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.standardAppearance.configureWithTransparentBackground()
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = barButton
     }
 }
--- Xiritori/View/FriendsView.swift
+++ Xiritori/View/FriendsView.swift
@@ -9,15 +9,43 @@
 import UIKit
 
 class FriendsView: UIView {
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.searchBarStyle = .minimal
+        searchBar.searchTextField.backgroundColor = .white
+        searchBar.searchTextField.returnKeyType = .search
+        searchBar.isOpaque = false
+        
+        return searchBar
+    }()
     
 // MARK: - INIT
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .white
+        self.addSubview(searchBar)
+        self.septupLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Constraints
+    private func septupLayout() {
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 10),
+            searchBar.heightAnchor.constraint(equalToConstant: 50),
+            searchBar.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+    }
+    
+    // Dismiss the keyboard when the view is touched.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.endEditing(true)
     }
     
 }
