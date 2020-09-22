c141e084966566534a17f7e4bda98a1217bdbd2f
Pedro Henrique Costa <henriquepcnews@gmail.com>
2020-09-22 12:03:22 -0300

    - Conserta arquivo PlayScreenController perdido por algum motivo

---
+++ Xiritori/Controller/PlayScreenController.swift
@@ -0,0 +1,55 @@
+//
+//  PlayScreenController.swift
+//  Xiritori
+//
+//  Created by Pedro Henrique Costa on 22/09/20.
+//  Copyright © 2020 Academy IFCE. All rights reserved.
+//
+
+import UIKit
+
+class PlayScreenController: UIViewController {
+
+    let playView = PlayScreenView()
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		self.view = playView
+		
+		setupComputerButton()
+    }
+    
+	func setupComputerButton() {
+		
+		let button = UIButton()
+		button.backgroundColor = .lightGreen
+		button.setTitleColor(.blazingBlack, for: .normal)
+		button.titleLabel?.font = UIFont(name: "MyMessyHandwriting", size: 32)
+		button.setTitle("VS COMPUTADOR", for: .normal)
+		button.addTarget(self, action: #selector(self.pressedComputer), for: .touchUpInside)
+		self.view.addSubview(button)
+		
+		button.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			button.widthAnchor.constraint(equalToConstant: 300),
+			button.heightAnchor.constraint(equalToConstant: 150),
+			button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -50),
+			button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -100)
+		])
+		
+		button.transform = button.transform.rotated(by: 150)
+		
+		let circlePath = UIBezierPath.init(arcCenter: CGPoint(x: 150, y: 150), radius: 150, startAngle: CGFloat(Double.pi), endAngle: 0.0, clockwise: true)
+		let circleShape = CAShapeLayer()
+		circleShape.path = circlePath.cgPath
+		button.layer.mask = circleShape
+	}
+	
+	@objc func pressedComputer() {
+		let gameController = ChatGameController()
+		gameController.modalPresentationStyle = .fullScreen
+		self.navigationController?.navigationBar.isHidden = true
+		self.navigationController?.pushViewController(gameController, animated: true)
+	}
+
+}
